add14efa3700001bb5259a881cbc3d93663282ab
Joe A. Spandrusyszyn <jspandrusyszyn@rocsoft.com>
2020-08-23 16:25:11 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# FoundryVTT-sheet5e-lockdown
+# Illandril's Character Sheet Lockdown (DnD5e)
+This is a module for Foundry Virtual Tabletop that prevents players from making changes to aspects of their character sheets that should change infrequently.
+
+It also completely hides the Background and Alignment inputs, the Currency Conversion button, and the Biography tab.
+
+Note that this is not a secure lockdown and players could get around the restrictions, so this is not a sufficient answer if your players are malicious. This is intended for games where the players are inexperienced (with D&D and/or with using computers) to prevent them from making mistakes (ex. accidentally adding/removing saving throw proficiency).
+
+The following are still editable to players...
+1. Current, +Temp, and +Max Hit Points
+1. Short/Long Rest Buttons
+1. Available Resource 1/2/3
+1. Death Saves
+1. Exhaustion
+1. Inspiration
+1. Feature Available Uses
+1. Available Spell Slots
+
+The GM can turn on editing for themselves through a Toggle Edit button in the character sheet title bar.
+
+# Installation
+1. Open the Configuration and Setup for your FoundryVTT server
+1. Open the Add-on Modules Tab
+1. Click the Install Module button
+1. In the Manifest URL input, specify https://github.com/illandril/FoundryVTT-sheet5e-lockdown/releases/latest/download/module.json
+1. Click Install
+1. Launch your world
+1. Log in as the GM
+1. Open the Settings tab
+1. Click Manage Modules
+1. Check the checkbox for Illandril's Character Sheet Lockdown (5e)
---
+++ init.js
@@ -0,0 +1,117 @@
+const CSS_PREFIX = 'illandril-sheet5e-lockdown--';
+const CSS_SHEET = CSS_PREFIX + 'sheet';
+const CSS_EDIT = CSS_PREFIX + 'edit';
+const CSS_LOCK = CSS_PREFIX + 'lock';
+
+const elementsToDisable = [
+  'input[name="name"]',
+  'input[name="data.details.race"]',
+  'input[name="data.attributes.hp.max"]',
+  'input[name="data.attributes.ac.value"]',
+  'input[name="data.attributes.speed.value"]',
+  'input[name="data.attributes.speed.special"]',
+  'input[name="data.attributes.init.value"]',
+  'input[name="data.resources.primary.label"]',
+  'input[name="data.resources.secondary.label"]',
+  'input[name="data.resources.tertiary.label"]',
+  'input[name="data.resources.primary.max"]',
+  'input[name="data.resources.secondary.max"]',
+  'input[name="data.resources.tertiary.max"]',
+  'input[name="data.resources.primary.sr"]',
+  'input[name="data.resources.secondary.sr"]',
+  'input[name="data.resources.tertiary.sr"]',
+  'input[name="data.resources.primary.lr"]',
+  'input[name="data.resources.secondary.lr"]',
+  'input[name="data.resources.tertiary.lr"]',
+  "input.ability-score",
+  "select.actor-size",
+  'input[name="data.traits.senses"]',
+  'input[name^="data.currency."]',
+  'select[name="data.attributes.spellcasting"]'
+].join(",");
+
+Hooks.on("renderActorSheet5eCharacter", (actorSheet5eCharacter, html, data) => {
+  const sheetElem = getSheetElem(html);
+  if (!sheetElem) {
+    return;
+  }
+
+  // Hide Background and Alignment
+  // Note: This isn't in the CSS because there is no way to reliably select the node we want to hide
+  forAll(
+    sheetElem,
+    'input[name="data.details.background"],input[name="data.details.alignment"]',
+    input => {
+      input.parentNode.style.display = "none";
+    }
+  );
+  if (sheetElem && !sheetElem.classList.contains(CSS_SHEET)) {
+    initialize(sheetElem);
+  }
+  makeEditable(sheetElem, isEditable(sheetElem));
+});
+
+function stopPropagation(event) {
+  event.stopPropagation();
+}
+
+function initialize(sheetElem) {
+  sheetElem.classList.add(CSS_SHEET);
+  if (game.user.isGM) {
+    const sheetHeader = sheetElem.querySelector(".window-header");
+    const sheetTitle = sheetHeader.querySelector(".window-title");
+
+    const editLink = document.createElement("a");
+    editLink.appendChild(faIcon("edit"));
+    editLink.appendChild(
+      document.createTextNode(
+        game.i18n.localize("illandril-sheet5e-lockdown.toggleEditable")
+      )
+    );
+    editLink.addEventListener("click", function() {
+      toggleEditable(sheetElem);
+    }, false);
+    editLink.addEventListener("dblclick", stopPropagation, false);
+    sheetHeader.insertBefore(editLink, sheetTitle.nextSibling);
+  }
+}
+
+function isEditable(sheetElem) {
+  return sheetElem.classList.contains(CSS_EDIT);
+}
+
+function toggleEditable(sheetElem) {
+  makeEditable(sheetElem, !isEditable(sheetElem));
+};
+
+function makeEditable(sheetElem, editable) {
+  if (editable) {
+    sheetElem.classList.remove(CSS_LOCK);
+    sheetElem.classList.add(CSS_EDIT);
+  } else {
+    sheetElem.classList.add(CSS_LOCK);
+    sheetElem.classList.remove(CSS_EDIT);
+  }
+  forAll(sheetElem, elementsToDisable, input => {
+    input.disabled = !editable;
+  });
+};
+
+function getSheetElem(html) {
+  let sheetElem = html[0];
+  while (sheetElem && !sheetElem.classList.contains("sheet")) {
+    sheetElem = sheetElem.parentNode;
+  }
+  return sheetElem;
+}
+
+function forAll(parent, selector, action) {
+  Array.prototype.forEach.call(parent.querySelectorAll(selector), action);
+}
+
+function faIcon(name) {
+  const icon = document.createElement("i");
+  icon.classList.add("fas");
+  icon.classList.add("fa-" + name);
+  return icon;
+}
