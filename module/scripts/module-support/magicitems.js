6e5d996755d07d1478fd4271c826666d3ef60ee2
Joe Spandrusyszyn <github@illandril.net>
2021-02-04 18:06:56 -0500

    Adjusted logging

--- module/scripts/module-support/magicitems.js
+++ module/scripts/module-support/magicitems.js
@@ -1,3 +1,5 @@
+import { log } from '../module.js';
+
 const MagicItemsSupport = {
   doesActorHaveSpells: (actor) => {
     if (window.MagicItems && window.MagicItems.actor) {
@@ -5,7 +7,7 @@ const MagicItemsSupport = {
         const magicitemsActor = window.MagicItems.actor(actor.id);
         return magicitemsActor && magicitemsActor.hasItemsSpells();
       } catch (e) {
-        console.error('Error checking for magicitems mod spells', e);
+        log.error('Error checking for magicitems mod spells', e);
       }
     }
     return false;
